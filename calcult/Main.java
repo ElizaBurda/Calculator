ace01ed157cdc8eabecde6f46339ba13c7197cc2
ElizaBurda <66063113+ElizaBurda@users.noreply.github.com>
2024-02-07 23:14:52 +0100

    Update Main.java

--- calcult/Main.java
+++ calcult/Main.java
@@ -4,13 +4,13 @@ public class Main {
     public static void main(String[] args) {
 
         Calculator calc = Calculator.instance.get();
-        int a = calc.plus.apply(1, 2);              //  BinaryOperator<Integer> plus
-        int b = calc.minus.apply(1, 1);             //  BinaryOperator<Integer> minus
+        int a = calc.plus.apply(1, 2);              //  а = 3
+        int b = calc.minus.apply(1, 1);             //  b = 0, на ноль делить не можем, соответственно ловим ошибку
         try {
-            int c = calc.devide.apply(a, b);        // BinaryOperator<Integer> devide
+            int c = calc.devide.apply(a, b);        
             calc.println.accept(c);                 // Деление на ноль нельзя
         } catch (ArithmeticException exception) {
-            System.out.println("Делить на ноль нельзя");
+            System.out.println("Делить на ноль нельзя");  // выводим наше предупреждающее сообщение 
         }
     }
 }
