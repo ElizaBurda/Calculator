3bb693900be7d12027122fc860d6317357423321
ElizaBurda <66063113+ElizaBurda@users.noreply.github.com>
2024-03-11 22:21:02 +0100

    Update Main.java

--- calcult/Main.java
+++ calcult/Main.java
@@ -4,13 +4,15 @@ public class Main {
     public static void main(String[] args) {
 
         Calculator calc = Calculator.instance.get();
-        int a = calc.plus.apply(1, 2);              //  а = 3
-        int b = calc.minus.apply(1, 1);             //  b = 0, на ноль делить не можем, соответственно ловим ошибку
-        try {
-            int c = calc.devide.apply(a, b);        
-            calc.println.accept(c);                 // Деление на ноль нельзя
-        } catch (ArithmeticException exception) {
-            System.out.println("Делить на ноль нельзя");  // выводим наше предупреждающее сообщение 
+        int a = calc.plus.apply(1, 2);
+        int b = calc.minus.apply(1,1);
+
+
+       if ( b != 0) {
+            int c = calc.devide.apply(a, b);
+            calc.println.accept(c);
+        } else if ( b == 0){
+            System.out.println("Делить на ноль нельзя");
         }
     }
 }
